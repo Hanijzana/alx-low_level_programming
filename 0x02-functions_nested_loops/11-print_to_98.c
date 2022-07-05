f6f78e87c209f8fe6102b4e8188133087beef50c
hanijzana <hanijzana2012@gmail.com>
2022-07-05 14:55:32 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,10 +2,9 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98,
- * 
- * @n: the number to start counting from n to 98
- * Return: Always 0.
+ * print_to_98 - prints all natural numbers from n to 98
+ *
+ * @n: n to 98
  */
 
 void print_to_98(int n)
