73d57be7aac80ef8ef8c218dfd39bae9b1dffeb2
hanijzana <hanijzana2012@gmail.com>
2022-07-07 10:35:17 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ *
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
+
