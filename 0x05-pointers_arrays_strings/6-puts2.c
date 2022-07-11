184241096820072ae893abe20353ae40f55575c4
hanijzana <hanijzana2012@gmail.com>
2022-07-11 12:40:24 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,8 +2,8 @@
 
 
 /**
- * puts2 - Prints every other character of a string, starting with the first character
- * @str: Pointer to the string to ptint
+ * puts2 - Prints every other character of a string 
+ * @str: Pointer to the string to be printed
  *
  * Return: void
  */
