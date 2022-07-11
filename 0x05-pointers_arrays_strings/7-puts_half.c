341c22161f274e940bdb3e2b396facacdbfe1482
hanijzana <hanijzana2012@gmail.com>
2022-07-11 12:25:44 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of a string
+ * @str: Pointer to the second half of the string
+ *
+ *Return: void
+ */
+
+void puts_half(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+		;
+
+	a++;
+	for (a /= 2; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+
+	_putchar('\n');
+}
+
