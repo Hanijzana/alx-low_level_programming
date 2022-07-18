a2d8d661939773279dbd0a02fdf42de118ca77a3
hanijzana <hanijzana2012@gmail.com>
2022-07-18 04:43:23 -0700

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _memcpy - copies memory area
+* @dest: Destination
+* @src: Source
+* @n: numbers of bytes to copy
+* Return: Dest
+*/
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	return (dest);
+}
+
