4fc36db81f91be1a7b02b115a49da2d79dad5a70
hanijzana <hanijzana2012@gmail.com>
2022-07-20 07:25:42 -0700

    Task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * is_prime_number - prime number or not
+ * @n: the numebr 
+ * @i: 
+ * Return: return 1 or 0
+ */
+
+int actual_prime(int n, int i);
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (actual_prime(n, n - 1));
+}
+
+/**
+ * actual_prime - prime number
+ * @n: the numebr
+ * @i: iteration
+ *
+ * Return: return 1 or 0
+ */
+
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+
+	if (n % i == 0 && i > 0)
+	{
+		return (0);
+	}
+	return (actual_prime(n, i - 1));
+}
+
