cfd2824d577d7583681a39ed8f967aab6ff0ab3c
hanijzana <hanijzana2012@gmail.com>
2022-07-26 13:22:54 -0700

    my 0x0B. C - malloc, free Tasks

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,46 +1,45 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * argstostr - concat all arguements in a prog
- * @ac: numbers of arguement
- * @av: arguement arrays
- *
- * Return: returns new string (SUCCESS), NULL (ERROR)
+ * argstostr - concatenate all arguments of your program with newline
+ * @ac: argument count
+ * @av: double pointer to array of strings passed to main
+ * Return: Null if fail, else return pointer to new string
  */
-
-
 
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, len;
-	char *str;
+	char *a, *retp;
+	int i, j, total;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			len++;
-		}
-		len++;
-	}
 
-	str = malloc(sizeof(char) * (len + 1));
-	if (str == NULL)
+	for (i = 0, total = 0; i < ac; i++)
+	{
+		for (j = 0; *(*(av + i) + j) != '\0'; j++, total++)
+			;
+		total++;
+	}
+	total++;
+
+	a = malloc(total * sizeof(char));
+	if (a == NULL)
 		return (NULL);
-	k = 0;
 
+	retp = a;
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			str[k] = av[i][j];
-			k++;
+			*a = av[i][j];
+			a++;
 		}
-		str[k] = '\n';
-		k++;
+		*a = '\n';
+		a++;
 	}
-	return (str);
+
+	return (retp);
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,44 +2,37 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - Creates a 2D array if integer
- * @width: The width of the array matrix
- * @height: Height of the array matrix
- *
- * Return: on success, return pointer to the Matrix
+ * alloc_grid - create a 2d integer grid using malloc.
+ * @width: int size width
+ * @height: int size height
+ * Return: Pointer to new grid
  */
 
 int **alloc_grid(int width, int height)
 {
-	int **arr;
+	int **grid;
 	int i, j;
 
-	if (height <= 0 || width <= 0)
+	if (width + height < 2 || width < 1 || height < 1)
 		return (NULL);
-	arr = (int **) malloc(sizeof(int *) * height);
-	if (arr == NULL)
+
+	grid = malloc(height * sizeof(*grid));
+	if (grid == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = (int *) malloc(sizeof(int) * width);
-		if (arr[i] == NULL)
+		grid[i] = malloc(width * sizeof(**grid));
+		if (grid[i] == NULL)
 		{
-			free(arr);
-			for (j = 0; j <= i; j++)
-			{
-				free(arr[j]);
-				return (NULL);
-			}
+			for (i--; i >= 0; i--)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
 		}
-	}
-
-	for (i = 0; i < height; i++)
-	{
 		for (j = 0; j < width; j++)
-		{
-			arr[i][j] = 0;
-		}
+			grid[i][j] = 0;
 	}
-	return (arr);
+
+	return (grid);
 }
